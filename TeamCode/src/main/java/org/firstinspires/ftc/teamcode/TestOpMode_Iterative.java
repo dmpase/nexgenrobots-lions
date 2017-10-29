cae6c3641ed6ef05042276b31f29eb0fc2fbe4b4
Doug Pase <dmpase@gmail.com>
2017-10-29 15:21:37 -0600

    sync'ing up with master.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode_Iterative.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode_Iterative.java
@@ -29,7 +29,6 @@
 
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -54,7 +53,7 @@ import static org.firstinspires.ftc.robotcontroller.external.samples.HardwarePus
  */
 
 @TeleOp(name="TestOpMode", group="Iterative Opmode")
-@Disabled
+// @Disabled
 public class TestOpMode_Iterative extends OpMode
 {
     // Declare OpMode members.
