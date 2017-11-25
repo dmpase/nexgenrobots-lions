5c3c5fb49fd27651a67a6fd22a7537e5b31ec541
Doug Pase <dmpase@gmail.com>
2017-11-25 13:12:37 -0700

    added MR distance sensor test.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DcMotorEnc.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DcMotorEnc.java
@@ -10,15 +10,17 @@ public class DcMotorEnc {
     public DcMotor motor   = null;
     public int     count   = 0;
     public int     target  = 0;
+    public double  min_pwr = 0.01;
     public double  max_pwr = 1.0;
     public double  scale   = 100;
     public double  power   = 0;
 
-    public DcMotorEnc(DcMotor motor, int target, double power, double scale)
+    public DcMotorEnc(DcMotor motor, int target, double max_pwr, double min_pwr, double scale)
     {
         this.motor   = motor;
         this.target  = target;
-        this.max_pwr = power;
+        this.max_pwr = max_pwr;
+        this.min_pwr = min_pwr;
         this.scale   = scale;
     }
 
@@ -32,6 +34,7 @@ public class DcMotorEnc {
         int c = motor.getCurrentPosition();
         power = (target - c)/scale;
         power = (power < -max_pwr) ? -max_pwr : (max_pwr < power) ? max_pwr : power;
+        power = (-min_pwr < power && power < min_pwr) ? 0 : power;
         DcMotor.RunMode mode = motor.getMode();
         motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motor.setPower(power);
