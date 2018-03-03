a63c976023d95246d54faeee21af066148bd27a5
dmpase <dmpase@gmail.com>
2018-03-03 05:42:39 -0700

    Updates to Lions new robot class.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LionRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LionRobot.java
@@ -52,6 +52,116 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
  */
 
 public class LionRobot extends GriffinRobot {
+    /**************************************************************************
+     *                           robot configuration                          *
+     **************************************************************************/
+
+
+    // Expansion Hub Portal 1
+    //     Expansion Hub 2
+    //         Motors
+    public static final String PORT_BOW_NAME        = "front left";             // Hub 2.Motors[0].Rev Robotics HD Hex
+    public static final String STBD_BOW_NAME        = "front right";            // Hub 2.Motors[1].Rev Robotics HD Hex
+    public static final String STBD_AFT_NAME        = "back right";             // Hub 2.Motors[2].Rev Robotics HD Hex
+    public static final String PORT_AFT_NAME        = "back left";              // Hub 2.Motors[3].Rev Robotics HD Hex
+
+    //         Servos
+    public static final String TAIL_NAME            = "tail";                   // Hub 2.Servos[0].Servo
+    public static final String BEAM_SWIVEL_NAME     = "beam swivel";            // Hub 2.Servos[3].Servo
+    public static final String BEAM_CLAW_NAME       = "beam claw";              // Hub 2.Servos[5].Servo
+
+    //         Analog Input Devices
+    public static final String STBD_BOW_IR_NAME     = "starboard bow ir range"; // Hub 2.Analog Input Devices[0].Analog Input
+    public static final String STBD_AFT_IR_NAME     = "starboard aft ir range"; // Hub 2.Analog Input Devices[1].Analog Input
+    public static final String PORT_BOW_IR_NAME     = "port bow ir range";      // Hub 2.Analog Input Devices[2].Analog Input
+    public static final String PORT_AFT_IR_NAME     = "port aft ir range";      // Hub 2.Analog Input Devices[3].Analog Input
+
+    //         I2C Bus 0
+    public static final String IMU0_NAME            = "imu 0";                  // Hub 2.I2C Bus 0[0].Rev Expansion Hub IMU
+
+    //         I2C Bus 1
+    public static final String STBD_MR_RANGE_NAME   = "starboard mr range";     // Hub 2.I2C Bus 1[0].MR Range Sensor
+
+    //         I2C Bus 2
+    public static final String PORT_MR_RANGE_NAME   = "port mr range";          // Hub 2.I2C Bus 2[0].MR Range Sensor
+
+    //         I2C Bus 3
+    public static final String REV_COLOR_RANGE_NAME = "rev color range";        // Hub 2.I2C Bus 3[0].Rev Color/Range Sensor
+
+
+    //     Expansion Hub 3
+    //         Motors
+    public static final String BEAM_DRIVE_NAME      = "beam drive";             // Hub 3.Motors[2].Rev Robotics HD Hex
+    public static final String LIFT_DRIVE_NAME      = "claw lift";              // Hub 3.Motors[3].Rev Robotics HD Hex
+
+    //         Servos
+    public static final String PORT_CLAW_NAME       = "port claw";              // Hub 3.Servos[0].Servo
+    public static final String STBD_CLAW_NAME       = "starboard claw";         // Hub 3.Servos[5].Servo
+
+    //         I2C Bus 0
+    public static final String IMU1_NAME            = "imu 1";                  // Hub 3.I2C Bus 0[0].Rev Expansion Hub IMU
+
+
+
+    /**************************************************************************
+     *                            robot parameters                            *
+     **************************************************************************/
+
+
+    // VuForia License Key
+    public static final String VUFORIA_LICENSE_KEY = "AWVXYZn/////AAAAGcG6g8XSSUMJsDaizcApOtsaA0fWzUQwImrdEn1MqH4JNqCzUwlyvEX0YALy7XyUeSpiANJkBg9kplUtcniUZKw8bF0dSpEfXZKXxn1yhbIohmpVmIK+Ngv1imYrkY6ePmvTfO2IpyQi5yO5ZmfSC8OzlH+XEMD0vRIXHMhxFpin7vTIHaoz8MEifSjRTznh1ZUSRnJfQ01KvMHEefES0kwhehlEKoqgpNMOYg0B5pV0bDDi9/Qh4eMR7sEk1GSx3QPxl/lYuZVcWSh8DutXv8oo9LhnbAaHTecCAR6gnNODow0WUAH2N9vxdLOjk2UfWVEJgqmHembIDHRzJN4fjcOECTFfLHIVmZ66GwgjPWxV";
+    public static final VuforiaLocalizer.CameraDirection CAMERA_DIRECTION = VuforiaLocalizer.CameraDirection.FRONT;
+
+
+    // Lift parameters
+    public static final double LIFT_POWER       = 0.75;
+    public static final int    LIFT_TARGET_HI   = 5000;
+    public static final int    LIFT_TARGET_INCH =  400;
+    public static final int    LIFT_TARGET_LO   =  200;
+    public static final int    LIFT_TARGET_SET  =  400;
+    public static final DcMotor.Direction LIFT_DIRECTION = DcMotor.Direction.FORWARD;
+
+
+    // Claw parameters
+    public static final double PORT_CLAW_OPENED = 0.90;
+    public static final double PORT_CLAW_CLOSED = 0.25;
+    public static final double STBD_CLAW_OPENED = 0.00;
+    public static final double STBD_CLAW_CLOSED = 0.65;
+
+
+    // Tail parameters
+    public static final double TAIL_POS_UP      = 0.00;
+    public static final double TAIL_POS_DN      = 0.90;
+
+
+    // Beam parameters
+    public static final double BEAM_POWER       = 0.40;
+    public static final int    BEAM_TARGET_IN   =    0;         // encoder clicks for full retraction
+    public static final int    BEAM_TARGET_OUT  = -15000;       // encoder clicks for full extension
+    public static final DcMotor.Direction BEAM_DIRECTION = DcMotor.Direction.FORWARD;
+    public static final double BEAM_CLAW_OPENED = 0.00;
+    public static final double BEAM_CLAW_CLOSED = 0.90;
+    public static final double BEAM_SWIVEL_UP   = 0.50;
+    public static final double BEAM_SWIVEL_DOWN = 0.00;
+
+    // Misc. motor parameters
+    public static final int    MOTOR_TARGET_TOLERANCE =   5;    // encoder clicks
+    public static final double MOTOR_LAG_SEC          =   0.25; // seconds
+    public static final long   MOTOR_LAG_MILLI        = 250;    // milliseconds
+
+
+    // Playing field surface constants
+    public static final double BALANCING_STONE  = 65;           // encoder clicks per linear inch
+    public static final double PLAYING_FIELD    = 75;           // encoder clicks per linear inch
+    public static final double ROTATION_RATE    = 10.0;         // encoder clicks per degree of rotation
+
+
+
+    /**************************************************************************
+     *                            OpMode functions                            *
+     **************************************************************************/
+
+
     // hardware map and telemetry from the OpMode class
     public HardwareMap hardwareMap = null;
     public Telemetry telemetry = null;
@@ -97,10 +207,10 @@ public class LionRobot extends GriffinRobot {
 
 
 
-
     /**************************************************************************
-     *                    drive functions for this robot                      *
+     *                             drive functions                            *
      **************************************************************************/
+
 
     // locomotion motors
     private DcMotor port_bow_drive = null;
@@ -316,6 +426,7 @@ public class LionRobot extends GriffinRobot {
 
     // run the drive motors to a given position,
     // DO reset the encoder before starting (i.e., always start from zero)
+    // this routine blocks until the desired location is reached
     private void run_to_position(int fl_tgt, int fr_tgt, int br_tgt, int bl_tgt, double power, int tolerance)
     {
         // cut all power to the motors
@@ -357,7 +468,7 @@ public class LionRobot extends GriffinRobot {
         }
 
         // sleep while the motors settle into their new position
-        sleep(LionConfig.MOTOR_LAG_MILLI);
+        sleep(MOTOR_LAG_MILLI);
 
         // kill all power to the motors
         port_bow_drive.setPower(0);
@@ -367,9 +478,11 @@ public class LionRobot extends GriffinRobot {
     }
 
 
+
     /**************************************************************************
-     *                     claw functions for this robot                      *
+     *                             claw functions                              *
      **************************************************************************/
+
 
     // claw and tail servos
     private Servo port_claw = null;
@@ -382,51 +495,74 @@ public class LionRobot extends GriffinRobot {
     @Override
     public void claw_init()
     {
-        port_claw = hardwareMap.get(Servo.class, LionConfig.PORT_CLAW);
-        port_claw.setDirection(Servo.Direction.FORWARD);
+        if (port_claw == null) {
+            port_claw = hardwareMap.get(Servo.class, PORT_CLAW_NAME);
+            port_claw.setDirection(Servo.Direction.FORWARD);
+        }
 
-        stbd_claw = hardwareMap.get(Servo.class, LionConfig.STBD_CLAW);
-        stbd_claw.setDirection(Servo.Direction.FORWARD);
+        if (stbd_claw == null) {
+            stbd_claw = hardwareMap.get(Servo.class, STBD_CLAW_NAME);
+            stbd_claw.setDirection(Servo.Direction.FORWARD);
+        }
 
-        lift = hardwareMap.get(DcMotor.class, LionConfig.LIFT_DRIVE);
-        lift.setDirection(LionConfig.LIFT_DIRECTION);
-        lift.setPower(0);
-        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        if (lift == null) {
+            lift = hardwareMap.get(DcMotor.class, LIFT_DRIVE_NAME);
+            lift.setDirection(LIFT_DIRECTION);
+            lift.setPower(0);
+            lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
     }
 
     @Override
     public void claw_open()
     {
-        port_claw.setPosition(LionConfig.PORT_CLAW_OPENED);
-        stbd_claw.setPosition(LionConfig.STBD_CLAW_OPENED);
+        port_claw.setPosition(PORT_CLAW_OPENED);
+        stbd_claw.setPosition(STBD_CLAW_OPENED);
     }
 
     @Override
     public void claw_close()
     {
-        port_claw.setPosition(LionConfig.PORT_CLAW_CLOSED);
-        stbd_claw.setPosition(LionConfig.STBD_CLAW_CLOSED);
+        port_claw.setPosition(PORT_CLAW_CLOSED);
+        stbd_claw.setPosition(STBD_CLAW_CLOSED);
     }
 
     @Override
     public void claw_raise(double height)
     {
+        lift.setPower(0);
+        lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        int target = (int) (LIFT_TARGET_LO + height * (LIFT_TARGET_HI - LIFT_TARGET_LO));
+        lift.setTargetPosition(target);
+        lift.setPower(LIFT_POWER);
     }
 
     @Override
     public void claw_stop()
     {
+        lift.setPower(0);
     }
 
     @Override
     public void claw_wait()
     {
+        double start = runtime.seconds();
+        while ( MOTOR_TARGET_TOLERANCE < Math.abs(lift.getTargetPosition() - lift.getCurrentPosition()) ) {
+            if (MOTOR_LAG_SEC < (runtime.seconds() - start)) break;
+        }
+
+        if ((runtime.seconds() - start) < MOTOR_LAG_SEC) {
+            sleep(MOTOR_LAG_SEC - (start - runtime.seconds()));
+        }
+
+        lift.setPower(0);
     }
 
 
+
     /**************************************************************************
-     *                     tail functions for this robot                      *
+     *                              tail functions                            *
      **************************************************************************/
 
 
@@ -437,19 +573,23 @@ public class LionRobot extends GriffinRobot {
     @Override
     public void tail_init()
     {
-        tail  = hardwareMap.get(Servo.class, LionConfig.TAIL);
-        tail.setDirection(Servo.Direction.FORWARD);
+        if (tail == null) {
+            tail = hardwareMap.get(Servo.class, TAIL_NAME);
+            tail.setDirection(Servo.Direction.FORWARD);
+        }
     }
 
     @Override
     public void tail_raise(double height)
     {
+        double pos = TAIL_POS_DN + height * (TAIL_POS_UP - TAIL_POS_DN);
+        tail.setPosition(height);
     }
 
 
 
     /**************************************************************************
-     *                     beam functions for this robot                      *
+     *                              beam functions                            *
      **************************************************************************/
 
 
@@ -464,46 +604,81 @@ public class LionRobot extends GriffinRobot {
     @Override
     public void beam_init()
     {
+        if (beam_claw == null) {
+            beam_claw = hardwareMap.get(Servo.class, BEAM_CLAW_NAME);
+            beam_claw.setDirection(Servo.Direction.FORWARD);
+        }
+
+        if (beam_swivel == null) {
+            beam_swivel = hardwareMap.get(Servo.class, BEAM_SWIVEL_NAME);
+            beam_swivel.setDirection(Servo.Direction.FORWARD);
+        }
+
+        if (beam_drive == null) {
+            beam_drive = hardwareMap.get(DcMotor.class, BEAM_DRIVE_NAME);
+            beam_drive.setDirection(DcMotor.Direction.FORWARD);
+            beam_drive.setPower(0);
+            beam_drive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            beam_drive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
     }
 
     @Override
     public void beam_open()
     {
+        beam_claw.setPosition(BEAM_CLAW_OPENED);
     }
 
     @Override
     public void beam_close()
     {
+        beam_claw.setPosition(BEAM_CLAW_CLOSED);
     }
 
     @Override
     public void beam_raise()
     {
+        beam_swivel.setPosition(BEAM_SWIVEL_UP);
     }
 
     @Override
     public void beam_lower()
     {
+        beam_swivel.setPosition(BEAM_SWIVEL_DOWN);
     }
 
     @Override
     public void beam_extend(double length)
     {
+        beam_drive.setPower(0);
+        int target = (int) (BEAM_TARGET_IN + length * (BEAM_TARGET_OUT - BEAM_TARGET_IN));
+        beam_drive.setTargetPosition(target);
+        beam_drive.setPower(BEAM_POWER);
     }
 
     @Override
     public void beam_stop()
     {
+        beam_drive.setPower(0);
     }
 
     @Override
     public void beam_wait()
     {
+        double start = runtime.seconds();
+        while ( MOTOR_TARGET_TOLERANCE < Math.abs(beam_drive.getTargetPosition() - beam_drive.getCurrentPosition()) ) {
+            if (MOTOR_LAG_SEC < (runtime.seconds() - start)) break;
+        }
+
+        if ((runtime.seconds() - start) < MOTOR_LAG_SEC) {
+            sleep(MOTOR_LAG_SEC - (start - runtime.seconds()));
+        }
     }
 
 
+
     /**************************************************************************
-     *                   vuforia functions for this robot                     *
+     *                           vuforia functions                            *
      **************************************************************************/
 
 
@@ -524,8 +699,8 @@ public class LionRobot extends GriffinRobot {
         cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
 
         vuforia_parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
-        vuforia_parameters.vuforiaLicenseKey = LionConfig.VUFORIA_LICENSE_KEY;
-        vuforia_parameters.cameraDirection = LionConfig.CAMERA_DIRECTION;
+        vuforia_parameters.vuforiaLicenseKey = VUFORIA_LICENSE_KEY;
+        vuforia_parameters.cameraDirection = CAMERA_DIRECTION;
         vuforia = ClassFactory.createVuforiaLocalizer(vuforia_parameters);
         relicTrackables = vuforia.loadTrackablesFromAsset("RelicVuMark");
         relicTemplate = relicTrackables.get(0);
@@ -553,8 +728,9 @@ public class LionRobot extends GriffinRobot {
     }
 
 
+
     /**************************************************************************
-     *                    sensor functions for this robot                     *
+     *                            sensor functions                            *
      **************************************************************************/
 
 
